b010ad0f9d213a7f644015d039843ef12b7fb641
Matt Yang <mdy834@gmail.com>
2021-05-16 20:28:12 -0400

    Created grid & highlights on mouseover

---
+++ index.js
@@ -0,0 +1,28 @@
+const container = document.getElementById('container')
+
+
+
+function createGrid(rows, columns){
+    for(let i = 0; i < rows; i++){
+        for(let j = 0; j < columns; j++){
+            let newDiv = document.createElement('div')
+            newDiv.classList.add('grid-item')
+            //newDiv.textContent = "hi"
+            container.appendChild(newDiv)
+        }
+    }
+}
+
+
+createGrid(16,16)
+
+
+const containerElements = document.querySelectorAll(".grid-item")
+
+console.log(containerElements)
+//containerElements[40].style.backgroundColor = "red"
+
+for(let i = 0; i < containerElements.length; i++){
+   containerElements[i].addEventListener("mouseover",function(){containerElements[i].style.backgroundColor = "red"})
+}
+
